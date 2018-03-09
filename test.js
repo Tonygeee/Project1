f046627c14e3376a87db106b782dcc710a4b2118
Ariel Solano <arielsolano@Ariels-MBP.localdomain>
2018-03-08 18:51:23 -0800

    google and eventbrite added

--- test.js
+++ test.js
@@ -8,74 +8,103 @@ twitter key: TyTpv7pvNW8efyJ2xsCckhA3O
 
 eventbrite: Key: V5EJ2TPSJGKB6YONZK
             Token: U5OPJ43N24OEA7SMURRR
+
+Google API Key: AIzaSyCoFPgedX41Fv_7LEYXo1QRS8SL2cIjj3Y
 */
-var city = ""; //filled with user input
+var placeSearch, autocomplete, city;
 
-$("#submit-button").on("click", function(event) {
-  event.preventDefault();
-  //grabbing value of users input
-  city = $("#city-input")
-    .val()
-    .trim();
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */ (document.getElementById("autocomplete")),
+    { types: [] }
+  );
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  console.log("Autocomplete information: ");
+  console.log(place);
+  city = place.address_components[0].long_name;
   console.log("User entered: " + city);
-  //Call Function/s that will display information
-  displayFoursquare();
-});
+  displayFoursquare2();
+  displayEventbrite();
+}
 
-//declare global var for venueName
-var venueName = "";
+// function geolocate() {
+//   autocomplete.setBounds(getBounds());
+// }
 
-function displayFoursquare() {
+// navigator.geolocation.getCurrentPosition(function(position) {
+//   var geolocation = {
+//     lat: position.coords.latitude,
+//     lng: position.coords.longitude,
+//   };
+//   var circle = new google.maps.Circle({
+//     center: geolocation,
+//     radius: position.coords.accuracy,
+//   });
+
+// $("#submit-button").on("click", function(event) {
+//   event.preventDefault();
+//   //grabbing value of users input
+//   city = $("#city-input")
+//     .val()
+//     .trim();
+//   console.log("User entered: " + city);
+//   //Call Function/s that will display information
+//   displayFoursquare();
+// });
+
+function displayFoursquare2() {
   //city variable filled in "#submit-button on click"
   $("#foursquare").empty();
   var queryURL =
-    "https://api.foursquare.com/v2/venues/search?near=" +
+    "https://api.foursquare.com/v2/venues/explore?near=" +
     city +
-    "&query=restaurant&v=20150214&m=foursquare&client_secret=WDJWVSV5OOXGZH5UU1D5Z4TCNTF3FFNJVQSN3ABBS25243K4&client_id=ZWTX3M0CQF4A34CXDBMUFHB1I4SDJV5CYRIJ3B1HQWNJZQKI&limit=10";
+    "&section=food&venuePhotos=1&m=foursquare&oauth_token=XA4FOIKVQSHXMH32T3J2BKV0EQKYL5EZZYXYF4P3ATQYD2SN&v=20180308&limit=10";
   //ajax request to get name and id
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function(response) {
-    console.log(response.response.venues);
-    var venues = response.response.venues;
-    for (var i = 0; i < venues.length; i++) {
-      console.log(response.response.venues[i].name);
-      var venueID = response.response.venues[i].id;
-      var VENUENAME = response.response.venues[i].name;
-      console.log("VENUE ID " + venueID);
-      getVenueImg(VENUENAME, venueID); //here we call the getVenueImg function and pass it two arguements: name and id (which are local scope to the first function)
+    console.log("foursquare object: ");
+    console.log(response);
+    for (var i = 0; i < response.response.groups[0].items.length; i++) {
+      $("#foursquare").append(
+        "<h1> " + response.response.groups[0].items[i].venue.name + "</h1>"
+      );
+      $("#foursquare").append(
+        "<img class = 'food-img'src = " +
+          response.response.groups[0].items[i].venue.photos.groups[0].items[0]
+            .prefix +
+          "original" +
+          response.response.groups[0].items[i].venue.photos.groups[0].items[0]
+            .suffix +
+          ">"
+      );
     }
   });
 }
+function displayEventbrite() {
+  var queryURL =
+    "https://www.eventbriteapi.com/v3/events/search/?location.address=" +
+    city +
+    "&token=BXIHDDMOSK4ACTSU43OP";
 
-function getVenueImg(name, id) {
-  //pass this function 2 arguements
-  var ImgURL =
-    "https://api.foursquare.com/v2/venues/" +
-    id +
-    "/photos?&oauth_token=XA4FOIKVQSHXMH32T3J2BKV0EQKYL5EZZYXYF4P3ATQYD2SN&v=20180306";
   $.ajax({
-    url: ImgURL,
+    url: queryURL,
     method: "GET",
-  }).then(function(image) {
-    console.log(image.response);
-    if (image.response.photos.items.length !== 0) {
-      //only display name/img if there is an image in the array
-      var imgDiv = $("<div>"); //create a div to keep the pair of name+img in each one
-      imgDiv.addClass("imgDiv"); //give it a class for styling later
-      var p = $("<p>").text(name);
-      var venueImg = $("<img>");
-      venueImg.attr(
-        "src",
-        image.response.photos.items[0].prefix +
-          "100x100" +
-          image.response.photos.items[0].suffix
-      );
-      venueImg.addClass("venueImg");
-      imgDiv.append(p);
-      imgDiv.append(venueImg);
-      $("#foursquare").prepend(imgDiv); //apend it to our foursquare div in html
-    }
+  }).then(function(response) {
+    console.log("Evenbrite object: ");
+    console.log(response);
+    $("#eventbrite").append(response.events[0].name.html);
+    $("#eventbrite").append(response.events[0].description.html);
   });
 }
