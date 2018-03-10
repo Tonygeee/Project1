db86e49de43b264f8c093066d3fb181163a002ed
Tony Gonzalez <tgonzalez887@yahoo.com>
2018-03-10 10:59:19 -0800

    fix

--- test.js
+++ test.js
@@ -8,63 +8,201 @@ twitter key: TyTpv7pvNW8efyJ2xsCckhA3O
 
 eventbrite: Key: V5EJ2TPSJGKB6YONZK
             Token: U5OPJ43N24OEA7SMURRR
+
+Google API Key: AIzaSyCoFPgedX41Fv_7LEYXo1QRS8SL2cIjj3Y
 */
-var city = ""; //filled with user input
+var placeSearch, autocomplete, city;
 
-$("#submit-button").on("click", function (event) {
-  event.preventDefault();
-  //grabbing value of users input
-  city = $("#city-input")
-    .val()
-    .trim();
+//init Autocomplete and fillInAddress are both used for the google autocomplete. keep them together.
+//THE SEARCH BAR YOU USE IN HTML HAS TO HAVE AN ID OF "autocomplete"
+// <input id="autocomplete" placeholder="Enter a city" type="text"></input>
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */ (document.getElementById("autocomplete")),
+    { types: [] }
+  );
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  console.log("Autocomplete information: ");
+  console.log(place);
+  city = place.formatted_address;
   console.log("User entered: " + city);
-  //Call Functions that will display information
-  displayFoursquare();
-  //   displayYelp();
-});
+  displayEventbriteMusic();
+  displayFoursquareCoffee();
+  displayFoursquareFood();
+  weather();
+}
 
-function displayFoursquare() {
+// function geolocate() {
+//   autocomplete.setBounds(getBounds());
+// }
+
+// navigator.geolocation.getCurrentPosition(function(position) {
+//   var geolocation = {
+//     lat: position.coords.latitude,
+//     lng: position.coords.longitude,
+//   };
+//   var circle = new google.maps.Circle({
+//     center: geolocation,
+//     radius: position.coords.accuracy,
+//   });
+
+// $("#submit-button").on("click", function(event) {
+//   event.preventDefault();
+//   //grabbing value of users input
+//   city = $("#city-input")
+//     .val()
+//     .trim();
+//   console.log("User entered: " + city);
+//   //Call Function/s that will display information
+//   displayFoursquare();
+// });
+
+function displayFoursquareFood() {
   //city variable filled in "#submit-button on click"
-
+  $("#foursquare").empty();
   var queryURL =
-    "https://api.foursquare.com/v2/venues/search?near=" +
+    "https://api.foursquare.com/v2/venues/explore?near=" +
     city +
-    "&query=coffee&v=20150214&m=foursquare&client_secret=WDJWVSV5OOXGZH5UU1D5Z4TCNTF3FFNJVQSN3ABBS25243K4&client_id=ZWTX3M0CQF4A34CXDBMUFHB1I4SDJV5CYRIJ3B1HQWNJZQKI&limit=10";
-
+    "&section=food&venuePhotos=1&m=foursquare&oauth_token=XA4FOIKVQSHXMH32T3J2BKV0EQKYL5EZZYXYF4P3ATQYD2SN&v=20180308&limit=10";
+  //ajax request to get name and id
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (response) {
-    console.log(response.response.venues);
-    $("#foursquare").append(response.response.venues[0].name);
-    /* To append the image. NOTE: doesn't currently work. Says We dont have access.
-    $("#foursquare").append(
-      "<img src='" +
-        response.response.venues[1].categories[0].icon.prefix +
-        response.response.venues[1].categories[0].icon.suffix +
-        "'>"
-    );
-    */
+  }).then(function(response) {
+    console.log("foursquare FOOD object: ");
+    console.log(response);
+    var link = "";
+    $("#foursquare").append(link);
+    for (var i = 0; i < response.response.groups[0].items.length; i++) {
+      link = response.response.groups[0].items[i].tips[0].canonicalUrl;
+      $("#foursquare").append(
+        "<h1><a target ='blank' href= " +
+          link +
+          ">" +
+          response.response.groups[0].items[i].venue.name +
+          "</a></h1>"
+      );
+      $("#foursquare").append(
+        "<a target = 'blank' href = " +
+          link +
+          "><img class = 'food-img'src = " +
+          response.response.groups[0].items[i].venue.photos.groups[0].items[0]
+            .prefix +
+          "original" +
+          response.response.groups[0].items[i].venue.photos.groups[0].items[0]
+            .suffix +
+          "></a>"
+      );
+    }
   });
 }
-function displayYelp() {
-  //TEST url for something to searching for. This will be changed later with user input
-  var URL =
-    "https://api.yelp.com/v3/businesses/search?term=restaurant&latitude=40.82783908257346&longitude=-74.10162448883057&client_id=LrOQ1nnyQzNiJYbVBjqJMg";
 
+function displayFoursquareCoffee() {
+  //city variable filled in "#submit-button on click"
+  $("#foursquare").empty();
+  var queryURL =
+    "https://api.foursquare.com/v2/venues/explore?near=" +
+    city +
+    "&section=coffee&venuePhotos=1&m=foursquare&oauth_token=XA4FOIKVQSHXMH32T3J2BKV0EQKYL5EZZYXYF4P3ATQYD2SN&v=20180308&limit=10";
+  //ajax request to get name and id
   $.ajax({
-    url: URL,
+    url: queryURL,
     method: "GET",
-    //headers: is to authenticate who we are instead of &API_KEY in the url above, Headers Authorization is used.
-    headers: {
-      Authorization:
-        "Bearer OOh_UrhHLZV11XrO0JYIM78p_s292XlDVJyjPea5fHknuURByqLe7UFo6MGt2KesAIltVL5vLonsq8j_UOLkjymV8maZsf_RZ37r",
-    },
-  })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
+  }).then(function(response) {
+    console.log("foursquare COFFEE object: ");
+    console.log(response);
+    var link = "";
+    $("#foursquare").append(link);
+    for (var i = 0; i < response.response.groups[0].items.length; i++) {
+      link = response.response.groups[0].items[i].tips[0].canonicalUrl;
+      $("#foursquare").append(
+        "<h1><a target ='blank' href= " +
+          link +
+          ">" +
+          response.response.groups[0].items[i].venue.name +
+          "</a></h1>"
+      );
+      $("#foursquare").append(
+        "<a target = 'blank' href = " +
+          link +
+          "><img class = 'food-img'src = " +
+          response.response.groups[0].items[i].venue.photos.groups[0].items[0]
+            .prefix +
+          "original" +
+          response.response.groups[0].items[i].venue.photos.groups[0].items[0]
+            .suffix +
+          "></a>"
+      );
+    }
+  });
+}
+function displayEventbriteMusic() {
+  var queryURL =
+    "https://www.eventbriteapi.com/v3/events/search/?location.address=" +
+    city +
+    "&sort_by=best&categories=103&token=BXIHDDMOSK4ACTSU43OP";
+
+  //codes: 101 is job fairs, 103 is concerts
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response) {
+    console.log("Evenbrite object: ");
+    console.log(response);
+    for (var i = 0; i < 10; i++) {
+      var link = response.events[i].url;
+      if (response.events[i].logo.url !== 0) {
+        $("#eventbrite").append(
+          "<a target = 'blank' href = " +
+            link +
+            "><h1>" +
+            response.events[i].name.text +
+            "</h1></a>"
+        );
+        // $("#eventbrite").append(response.events[0].description.text);
+        $("#eventbrite").append(
+          "<a target = 'blank' href = " +
+            link +
+            "><img src= " +
+            response.events[i].logo.url +
+            "></a>"
+        );
+      }
+    }
+  });
+}
+function weather() {
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?" +
+      "q=" +
+      city +
+      "&units=imperial&appid=9d09809a4b038ee946dc9c53ea322c14",
+    method: "GET",
+  }).then(function(response) {
+    console.log(response);
+    console.log("Name: " + response.name);
+    console.log("Max Current Temp in this city: " + response.main.temp_max);
+    console.log("Min Current Temp in this city: " + response.main.temp_min);
+    $("#weather").append(
+      response.name +
+        "<br>" +
+        " Max Current Temp in this city: " +
+        response.main.temp_max +
+        "<br>" +
+        " Min Current Temp in this city: " +
+        response.main.temp_min +
+        "<br>"
+    );
+  });
 }
